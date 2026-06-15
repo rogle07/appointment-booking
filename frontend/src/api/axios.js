cce9afe2233a64f976728a9ffec48a98de756aef
ykumar778899@gmail.com <ykumar778899@gmail.com>
2026-06-15 23:39:38 +0530

    nefdfd

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://appointment-booking-fbgw.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://appointment-booking-fbgw.onrender.com' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
