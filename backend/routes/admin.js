e1d7327c4e2c21afa6120fc30b22a04188ce048d
ykumar778899@gmail.com <ykumar778899@gmail.com>
2026-06-15 23:24:47 +0530

    fix admin appointment import

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Appointment = require('../models/Appointment');
+const Appointment = require('../models/appointment');
 const Service = require('../models/Service');
 const auth = require('../middleware/auth');
 
