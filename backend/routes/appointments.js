d73f418da96511f4b790bbbbefcaa3294c73aea2
ykumar778899@gmail.com <ykumar778899@gmail.com>
2026-06-15 23:16:05 +0530

    neonedf

--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Appointment = require('../models/Appointment');
+const Appointment = require('../models/appointment');
 const auth = require('../middleware/auth');
 
 // Book appointment (customer)
